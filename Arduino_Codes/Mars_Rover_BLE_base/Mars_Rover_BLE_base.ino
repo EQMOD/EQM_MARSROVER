ace92fe7b4c6fa9ddb1391a5c5b828288cbe5fd8
EQMOD <eqmod68@gmail.com>
2023-10-23 22:10:07 +0800

    Update Mars_Rover_BLE_base.ino

--- Arduino_Codes/Mars_Rover_BLE_base/Mars_Rover_BLE_base.ino
+++ Arduino_Codes/Mars_Rover_BLE_base/Mars_Rover_BLE_base.ino
@@ -1,3 +1,30 @@
+
+//---------------------------------------------------------------------
+// Copyright © 2020 Raymund Sarmiento
+//
+// Permission is hereby granted to use this Software for any purpose
+// including combining with commercial products, creating derivative
+// works, and redistribution of source or binary code, without
+// limitation or consideration. Any redistributed copies of this
+// Software must include the above Copyright Notice.
+//
+// THIS SOFTWARE IS PROVIDED "AS IS". THE AUTHOR OF THIS CODE MAKES NO
+// WARRANTIES REGARDING THIS SOFTWARE, EXPRESS OR IMPLIED, AS TO ITS
+// SUITABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+//
+//  DISCLAIMER:
+
+//  You can use the information on this site COMPLETELY AT YOUR OWN RISK.
+//  The modification steps and other information on this site is provided
+//  to you "AS IS" and WITHOUT WARRANTY OF ANY KIND, express, statutory,
+//  implied or otherwise, including without limitation any warranty of
+//  merchantability or fitness for any particular or intended purpose.
+//  In no event the author will  be liable for any direct, indirect,
+//  punitive, special, incidental or consequential damages or loss of any
+//  kind whether or not the author  has been advised of the possibility
+//  of such loss.
+//---------------------------------------------------------------------
+
 #include <Wire.h>
 #include <Servo.h>
 //#include <phyphoxBle.h> 
